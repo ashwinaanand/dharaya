dbac375b2cc5afc4f0b78acdb01e0f63581221cd
ashwina anand <ashwinaanand1214@gmail.com>
2026-03-23 01:12:13 +0530

    Upgrade DHARAYA: Professional landing page, seeded database, enhanced Dashboard & PollutionHeatmap, deployment files

--- dharaya/server.js
+++ dharaya/server.js
@@ -16,10 +16,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGODB_URI)
 .then(() => {
   console.log('✓ MongoDB connected successfully');
 })
